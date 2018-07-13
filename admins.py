9ca0a1c5f57f34c04d1b90ee0226cd830835f3b8
Matheus Protzen <matheus12.kp@gmail.com>
2018-07-13 01:16:47 -0300

    Detalhes

--- admins.py
+++ admins.py
@@ -219,7 +219,7 @@ def reboot(bot, update):
 @restricted
 def update(bot, update):
     bot.sendChatAction(chat_id=update['message']['chat']['id'], action=ChatAction.TYPING)
-    subprocess.call(["cd", "Documents/SAPU/"])
+    subprocess.call(["cd", "Documents/SAPU/"], shell=True)
     subprocess.call(["git", "pull"])
     bot.send_message(chat_id=update['message']['chat']['id'], text="O bot foi atualizado para a versão mais recente.",
                      parse_mode=ParseMode.HTML)
