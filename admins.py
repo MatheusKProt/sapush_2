be1617aec425dac374d0ccc4eb204fbcf56a64bc
Matheus Protzen <matheus12.kp@gmail.com>
2018-07-22 01:37:11 -0300

    Detalhe

--- admins.py
+++ admins.py
@@ -232,7 +232,7 @@ def statistics(bot, update):
 
     ligado = "Ligado há %d dias, %d horas, %d minutos e %d segundos" % (dias_ligado, horas_ligado, minutos_ligado, segundos_ligado)
     memoria_total = memoria.total / 1000000000
-    memoria_usada = memoria.used / 1000000000
+    memoria_usada = memoria.active / 1000000000
     memoria_disponivel = memoria.available / 1000000000
     disco_total = disco.total / 1073741824
     disco_usado = disco.used / 1073741824
