6680bbcb5665d875f75a2dce782b730e25ca79c6
Matheus Protzen <matheus12.kp@gmail.com>
2018-08-21 18:39:12 -0300

    Detalhe

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 def token():
-    return "557724029:AAH7nd9k6T6Auqkobv23zMGeiKQ7NKv8Eq8"
+    return "578171262:AAE1XZs99ZVqANBVIERj4CUpuC55yliyupY"
 
 
 def bing(key):
@@ -7,3 +7,7 @@ def bing(key):
         return "07021ed50fb04b89bacf60c4eec35e51"  # Matheus
     else:
         return "083fcbb8514b44e38830568fd2ab5fcf"  # Lucas
+
+
+def apiai():
+    return "4d525e294fb449c582ce3e6f34e88bea"
