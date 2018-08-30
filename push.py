27f54d5d9b119ddf1b2c084efb7c1cf8811c7b30
Lucas Ferreira <lucas@peferreira.com>
2018-08-30 00:19:38 -0300

    Update push.py

--- push.py
+++ push.py
@@ -76,13 +76,12 @@ def get_notas(bot, update, user):
     for detalhe in notas_detalhe:
         resumo = session.query(db.NotasResumo).filter_by(user_id=user.telegram_id, materia=detalhe[8]).first()
         if not resumo:
-            pass
-             try:
+            try:
                 bot.send_message(chat_id=user.telegram_id,
-                                  text=messages.push_grades_null(user.first_name, util.formata_nome_materia(detalhe[8])[:-1], detalhe[1]),
-                                  parse_mode=ParseMode.HTML)
-                except Exception as error:
-                    main.error_callback(bot, update, error)
+                                 text=messages.push_grades_null(user.first_name, util.formata_nome_materia(detalhe[8])[:-1], detalhe[1]),
+                                 parse_mode=ParseMode.HTML)
+            except Exception as error:
+                main.error_callback(bot, update, error)
         else:
             detalhe_sapu = session.query(db.NotasDetalhe).filter_by(materia=resumo.id,
                                                                     descricao=str(detalhe[0]),
@@ -95,21 +94,19 @@ def get_notas(bot, update, user):
                                                                         descricao=str(detalhe[0]),
                                                                         data=str(detalhe[1])).first()
                 if detalhe_sapu:
-                    pass
-                     try:
+                    try:
                         bot.send_message(chat_id=user.telegram_id,
-                                          text=messages.push_grades(user.first_name, util.formata_nome_materia(resumo.materia),
-                                                                    float(util.verifica_vazio(detalhe[3])),
-                                                                    resumo.media,
-                                                                    util.formata_notas_msg(detalhe[3])),
-                                          parse_mode=ParseMode.HTML)
-                     except Exception as error:
-                         main.error_callback(bot, update, error)
+                                         text=messages.push_grades(user.first_name, util.formata_nome_materia(resumo.materia),
+                                                                   float(util.verifica_vazio(detalhe[3])),
+                                                                   resumo.media,
+                                                                   util.formata_notas_msg(detalhe[3])),
+                                         parse_mode=ParseMode.HTML)
+                    except Exception as error:
+                        main.error_callback(bot, update, error)
                 else:
-                    pass
-                     bot.send_message(chat_id=user.telegram_id,
-                                      text=messages.push_provas(user.first_name, util.formata_nome_materia(resumo.materia)),
-                                      parse_mode=ParseMode.HTML)
+                    bot.send_message(chat_id=user.telegram_id,
+                                     text=messages.push_provas(user.first_name, util.formata_nome_materia(resumo.materia)),
+                                     parse_mode=ParseMode.HTML)
     dao.set_notas(user, notas_resumo, notas_detalhe, bot)
     session.close()
 
