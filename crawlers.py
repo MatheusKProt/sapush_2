d31ef438a1a966abe978ab79916662f9671e534d
Matheus Protzen <matheus12.kp@gmail.com>
2019-04-12 17:40:11 -0300

    Update crawlers.py

--- crawlers.py
+++ crawlers.py
@@ -21,7 +21,7 @@ headers = {
     "Accept-Encoding": 'br, gzip, deflate',
     "Origin": 'https://sapu.ucpel.edu.br',
     'Connection': 'keep-alive',
-    'User-Agent': randint(0, 9999999999),
+    'User-Agent': str(randint(0, 9999999999)),
     #  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1 Safari/605.1.15',
     'X-Requested-With': 'XMLHttpRequest'
 }
